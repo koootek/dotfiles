f797f89360c60cadd74a5c5c5d573e3642e8c7be
koootek <190116302+koootek@users.noreply.github.com>
2025-10-08 23:16:46 +0200

    Add muted option to system volume display

--- .config/sway/swaybar.sh
+++ .config/sway/swaybar.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
+# Requires NerdFonts
+separator="\u2502"
+
 while true; do
-	volume=$(awk -F"[][]" '/Left:/ { print $2 }' <(amixer sget Master))
+	amixer=$(amixer sget Master)
+	volume=$(awk -F"[][]" '/Left:/ { print $2 }' <<<"$amixer")
+	muted=$(awk '/\[off\]/ { print "true"; exit }' <<<"$amixer")
 	date=$(date +'%Y-%m-%d %X')
-	echo "Vol: $volume | $date"
+	if [[ "$muted" == "true" ]]; then
+		bartext="\ueee8  Muted"
+	else
+		bartext="\uf028  $volume"
+	fi
+	echo -e "$bartext $separator $date "
 	sleep 1
 done
